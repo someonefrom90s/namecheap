f3c0cdf83e2cf732590755f54a9c363588e3d666
Kaan Kahraman <kaan.kahraman@commencis.com>
2020-04-26 01:53:27 +0300

    removed reduntant parts

--- main.tf
+++ main.tf
@@ -22,29 +22,3 @@ module "repositories" {
   gh_token  = "${var.gh_token}"
   gh_org    = "${var.gh_org}"
 }
-
-## Add a collaborator to a repository
-#resource "github_repository_collaborator" "users_repos" {
-#  count      = "${length(var.gh_users)}"
-#  repository = "${var.gh_users[count.index]}_repo"
-#  username   = "${var.gh_users[count.index]}"
-#  permission = "push"
-#  depends_on = ["github_repository.users_repos"]
-#}
-#
-## Create labels
-#resource "github_issue_label" "users_repos" {
-#  count      = "${length(var.gh_users) * length(var.gh_labels) * var.gh_labels_enable}"
-#  repository = "${var.gh_users[count.index / length(var.gh_labels)]}_repo"
-#  name       = "${var.gh_labels[(count.index) % length(var.gh_labels)]}"
-#  color      = "${var.gh_color_labels}"
-#  depends_on = ["github_repository.users_repos"]
-#}
-#
-#resource "github_issue_label" "users_repost" {
-#  count      = "${length(var.gh_users) * var.gh_task_count  * var.gh_labels_enable}"
-#  repository = "${var.gh_users[count.index / var.gh_task_count]}_repo"
-#  name       = "Task-${(count.index) % var.gh_task_count + 1}"
-#  color      = "${var.gh_color_tasks}"
-#  depends_on = ["github_repository.users_repos"]
-#}
